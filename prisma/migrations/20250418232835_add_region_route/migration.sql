ad1ded7abbab754b4790ea53eea7456586e794b3
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-04-18 17:39:54 -0600

    Revert "add region route"
    This reverts commit e54a9280eeb3067308f449efb7aae3a0a2b51a4a.

--- prisma/migrations/20250418232835_add_region_route/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "user_settings" ADD COLUMN     "last_checked" TIMESTAMPTZ(0),
-ADD COLUMN     "region" VARCHAR(255),
-ADD COLUMN     "user_picked" BOOLEAN NOT NULL DEFAULT false;

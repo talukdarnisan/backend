ec8a973c2e6f6e834f376ed17a3916211c49b459
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-05-05 10:08:14 -0600

    make trakt client id optional

--- server/utils/trakt.ts
+++ server/utils/trakt.ts
@@ -1,14 +1,14 @@
 import Trakt from 'trakt.tv';
 const traktKeys = useRuntimeConfig().trakt;
 
-if (!traktKeys) {
-  throw new Error('Missing TraktKeys info ERROR: ' + JSON.stringify(traktKeys));
+let trakt: Trakt | null = null;
+
+if (traktKeys?.clientId && traktKeys?.clientSecret) {
+  const options = {
+    client_id: traktKeys.clientId,
+    client_secret: traktKeys.clientSecret,
+  };
+  trakt = new Trakt(options);
 }
 
-const options = {
-  client_id: traktKeys.clientId,
-  client_secret: traktKeys.clientSecret,
-};
-
-const trakt = new Trakt(options);
 export default trakt;
